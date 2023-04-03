a255c94b2f21e4af5c17bdb7d68833cba0355f79
drKonarev <dr.konarev@outlook.com>
2023-04-03 08:25:58 +0300

    delete useless error classes, add tests for all fields, fix some bugs2

--- src/test/java/ru/practicum/shareit/booking/BookingRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.annotation.DirtiesContext;
 import ru.practicum.shareit.item.model.Item;
@@ -118,7 +119,6 @@ class BookingRepositoryTest {
                 .findPastBookingByBooker_IdAndItem_Id(2L, 1L);
 
         assertFalse(actualList.isEmpty());
-
         assertEquals(1, actualList.size());
         assertEquals(2L, actualList.get(0).getId());
     }
@@ -135,7 +135,7 @@ class BookingRepositoryTest {
 
     @Test
     void findBookingByItem_Owner_IdTest() {
-        List<Booking> actualList = bookingRepository.findBookingByItem_Owner_Id(2L, Pageable.unpaged()).toList();
+        List<Booking> actualList = bookingRepository.findBookingByItem_Owner_Id(2L, PageRequest.of(0,10)).toList();
 
         assertFalse(actualList.isEmpty());
         assertEquals(2, actualList.size());
