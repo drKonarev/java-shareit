ae7d91b689677f605dd5ebfa961f699988568146
drKonarev <dr.konarev@outlook.com>
2023-04-03 23:31:09 +0300

    delete useless error classes, add tests for all fields, fix some bug98

--- src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/ItemServiceImpl.java
@@ -24,7 +24,6 @@ import ru.practicum.shareit.user.UserRepository;
 import ru.practicum.shareit.user.model.User;
 
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
