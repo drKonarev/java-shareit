4d86d134dcee57a50ba7d4e2eb49729068d44587
drKonarev <dr.konarev@outlook.com>
2023-04-05 08:56:54 +0300

    delete useless error classes, add tests for all fields, fix some bugs g9es2q22ds final round

--- src/test/java/ru/practicum/shareit/booking/BookingRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingRepositoryTest.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.booking;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -32,13 +33,64 @@ class BookingRepositoryTest {
     @Autowired
     private TestEntityManager em;
 
-    User user1 = new User(null, "testName", "test1@mail.ru");
-    User user2 = new User(null, "testNam2", "test2@mail.com");
-    Item item1 = new Item(null, user1, "iTem1 purrrrr", "desc1 pam pam pam", true, null);
-    Item item2 = new Item(null, user2, "iTem2 NAme", "desc2 CAAAAT", false, null);
-    Booking booking1 = new Booking(null, item1, user1, LocalDateTime.now().minusHours(2), LocalDateTime.now().plusHours(6), BookingStatus.WAITING);
-    Booking booking2 = new Booking(null, item2, user2, LocalDateTime.now().minusDays(2), LocalDateTime.now().minusHours(6), BookingStatus.APPROVED);
-    Booking booking3 = new Booking(null, item2, user2, LocalDateTime.now().plusDays(2), LocalDateTime.now().plusHours(56), BookingStatus.APPROVED);
+    @BeforeEach
+    public void setUp() {
+
+        User user1 = new User();
+        User user2 = new User();
+
+        user1.setName("testName1");
+        user1.setEmail("test1@mail.com");
+        user2.setName("testName2");
+        user2.setEmail("test25@mail.ru");
+
+
+        Item item1 = new Item();
+        Item item2 = new Item();
+
+        item1.setName("iTem1 purrrrr");
+        item1.setAvailable(true);
+        item1.setOwner(user1);
+        item1.setDescription("desc1 pam pam pam");
+
+        item2.setName("iTem2 NAme");
+        item2.setAvailable(false);
+        item2.setOwner(user2);
+        item2.setDescription("desc2 CAAAAT");
+
+
+        Booking booking1 = new Booking();
+        Booking booking2 = new Booking();
+        Booking booking3 = new Booking();
+
+        booking1.setItem(item1);
+        booking1.setBooker(user1);
+        booking1.setStatus(BookingStatus.WAITING);
+        booking1.setStart(LocalDateTime.now().minusHours(2));
+        booking1.setEnd(LocalDateTime.now().plusHours(5));
+
+        booking2.setItem(item2);
+        booking2.setBooker(user1);
+        booking2.setStatus(BookingStatus.APPROVED);
+        booking2.setStart(LocalDateTime.now().minusDays(1));
+        booking2.setEnd(LocalDateTime.now().minusHours(6));
+
+        booking3.setItem(item2);
+        booking3.setBooker(user2);
+        booking3.setStatus(BookingStatus.APPROVED);
+        booking3.setStart(LocalDateTime.now().plusDays(1));
+        booking3.setEnd(LocalDateTime.now().plusHours(36));
+
+        em.persist(user1);
+        em.persist(user2);
+        em.persist(item1);
+        em.persist(item2);
+        em.persist(booking1);
+        em.persist(booking2);
+        em.persist(booking3);
+
+    }
+
 
     @Test
     void contextLoads() {
@@ -48,14 +100,7 @@ class BookingRepositoryTest {
 
     @Test
     void getLastBookingByItemIdTest() {
-        em.persist(user1);
-        em.persist(user2);
-        em.persist(item1);
-        em.persist(item2);
-        em.persist(booking1);
-        em.persist(booking2);
-        em.persist(booking3);
-        System.out.println(bookingRepository.findAll());
+        System.out.println("\n" + bookingRepository.findAll() + "\n");
         Optional<Booking> actual = bookingRepository.getLastBookingByItemId(2);
 
         assertTrue(actual.isPresent());
@@ -65,16 +110,8 @@ class BookingRepositoryTest {
 
     @Test
     void getNextBookingByItemIdTest() {
-        em.persist(user1);
-        em.persist(user2);
-        em.persist(item1);
-        em.persist(item2);
-        em.persist(booking1);
-        em.persist(booking2);
-        em.persist(booking3);
-        System.out.println(bookingRepository.findAll());
+        System.out.println("\n" + bookingRepository.findAll() + "\n");
         Optional<Booking> actual = bookingRepository.getNextBookingByItemId(2);
-        System.out.println(actual);
         assertTrue(actual.isPresent());
         assertEquals(3L, actual.get().getId());
         assertEquals(BookingStatus.APPROVED, actual.get().getStatus());
@@ -83,16 +120,9 @@ class BookingRepositoryTest {
 
     @Test
     void findPastBookingByBooker_IdAndItem_IdTest() {
-        em.persist(user1);
-        em.persist(user2);
-        em.persist(item1);
-        em.persist(item2);
-        em.persist(booking1);
-        em.persist(booking2);
-        em.persist(booking3);
-        System.out.println(bookingRepository.findAll());
+        System.out.println("\n" + bookingRepository.findAll() + "\n");
         List<Booking> actualList = bookingRepository
-                .findPastBookingByBooker_IdAndItem_Id(2L, 2L);
+                .findPastBookingByBooker_IdAndItem_Id(2L, 1L);
 
         assertFalse(actualList.isEmpty());
         assertEquals(1, actualList.size());
@@ -101,37 +131,23 @@ class BookingRepositoryTest {
 
     @Test
     void findAllByBooker_IdOrderByStartDescTest() {
-        em.persist(user1);
-        em.persist(user2);
-        em.persist(item1);
-        em.persist(item2);
-        em.persist(booking1);
-        em.persist(booking2);
-        em.persist(booking3);
-        System.out.println(bookingRepository.findAll());
-        List<Booking> actualList = bookingRepository.findAllByBooker_IdOrderByStartDesc(2L, Pageable.unpaged()).toList();
-        System.out.println("\n" + actualList);
+        System.out.println("\n" + bookingRepository.findAll() + "\n");
+        List<Booking> actualList = bookingRepository.findAllByBooker_IdOrderByStartDesc(1L, Pageable.unpaged()).toList();
         assertFalse(actualList.isEmpty());
         assertEquals(2, actualList.size());
-        assertEquals(3L, actualList.get(0).getId());
+        assertEquals(1L, actualList.get(0).getId());
         assertEquals(2L, actualList.get(1).getId());
     }
 
     @Test
     void findBookingByItem_Owner_IdTest() {
-        em.persist(user1);
-        em.persist(user2);
-        em.persist(item1);
-        em.persist(item2);
-        em.persist(booking1);
-        em.persist(booking2);
-        em.persist(booking3);
-        System.out.println(bookingRepository.findAll());
-        List<Booking> actualList = bookingRepository.findBookingByItem_Owner_Id(user2.getId(), Pageable.unpaged()).toList();
-
+        System.out.println("\n" + bookingRepository.findAll() + "\n");
+        List<Booking> actualList = bookingRepository.findBookingByItem_Owner_Id(2L, Pageable.unpaged()).toList();
+        System.out.println("\n" + "!!!!!!" + "\n" + actualList);
         assertFalse(actualList.isEmpty());
         assertEquals(2, actualList.size());
-        assertEquals(item2.getId(), actualList.get(0).getItem().getId());
+        assertEquals(3L, actualList.get(0).getId());
+        assertEquals(2L, actualList.get(1).getId());
     }
 
 
