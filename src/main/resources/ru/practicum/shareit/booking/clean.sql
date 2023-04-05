0ad56d7d1c59d3185517202333410e25f62624b3
drKonarev <dr.konarev@outlook.com>
2023-04-05 08:32:33 +0300

    delete useless error classes, add tests for all fields, fix some bugs g9282q22dsj

--- src/main/resources/ru/practicum/shareit/booking/clean.sql
+++
@@ -1,5 +0,0 @@
-drop table if exists bookings cascade;
-drop table if exists comments cascade;
-drop table if exists Item_requests cascade;
-drop table if exists Items cascade;
-drop table if exists Users cascade;
