69074c0627ffe75ecd3e0eb8813c1fd0053d3ac5
drKonarev <dr.konarev@outlook.com>
2023-04-15 11:07:52 +0300

    one one more more

--- src/main/java/ru/practicum/shareit/error/AccessOrAvailableException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.error;
-
-public class AccessOrAvailableException extends RuntimeException {
-    public AccessOrAvailableException(String message) {
-        super(message);
-    }
-}
--- src/main/java/ru/practicum/shareit/markers/Create.java
+++
@@ -1,4 +0,0 @@
-package ru.practicum.shareit.markers;
-
-public interface Create {
-}
